5b80d48fad27b51624aa476edaa992e84dcb8bde
StrongCod3r <strong.cod3r@gmail.com>
2025-11-02 00:30:52 +0100

    Delete Shell

--- MauiWebView/App.xaml.cs
+++ MauiWebView/App.xaml.cs
@@ -7,11 +7,7 @@ namespace MauiApp2
         public App()
         {
             InitializeComponent();
-        }
-
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new MainPage();
         }
     }
 }
--- MauiWebView/MainPage.xaml.cs
+++ MauiWebView/MainPage.xaml.cs
@@ -1,39 +1,51 @@
 ﻿using MauiApp2.Services;
+using MauiPageFullScreen;
 
 namespace MauiApp2
 {
+    
     public partial class MainPage : ContentPage
     {
-        private readonly IScreenLockService _screenLockService;
+        private IScreenLockService _screenLockService;
+        private string webUrl = "http://";
 
-        public MainPage(IScreenLockService screenLockService)
+        public MainPage()
         {
             InitializeComponent();
-            _screenLockService = screenLockService;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-            // Activate wake lock to keep screen on
-            _screenLockService.KeepScreenOn(true);
+        
+        // Resolve the service here when the page appears
+   _screenLockService ??= IPlatformApplication.Current?.Services?.GetService<IScreenLockService>();
+  
+       // Activate wake lock to keep screen on
+      _screenLockService?.KeepScreenOn(true);
+
+#if !WINDOWS
+            Controls.FullScreen();
+#endif
+     }
+
+     protected override void OnDisappearing()
+     {
+  base.OnDisappearing();
+          // Release wake lock when leaving the page
+   _screenLockService?.KeepScreenOn(false);
         }
 
-        protected override void OnDisappearing()
+        private async void FloatingButton_Clicked(object sender, EventArgs e)
         {
-            base.OnDisappearing();
-            // Release wake lock when leaving the page
-            _screenLockService.KeepScreenOn(false);
-        }
+     //OptionsPanel.IsVisible = !OptionsPanel.IsVisible;
+         string result = await DisplayPromptAsync("Setup", "What's your web?", initialValue: webUrl);
 
-        private void Add_Clicked(object sender, EventArgs e)
+     if (!string.IsNullOrEmpty(result))
         {
-            // Implement Add functionality
-        }
-
-        private void Save_Clicked(object sender, EventArgs e)
-        {
-            // Implement Save functionality
+         webUrl = result;
+         WebViewBrowser.Source = result;
+      }
         }
     }
 }
